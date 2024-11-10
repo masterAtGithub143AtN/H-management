f09f94254a856678e7c062728558df2cfb2705a6
saket suman <12mdpur@gmail.com>
2024-11-10 19:03:29 +0530

    issue in using component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Admin from './pages/Admin/Admin';
-
+// Remove the duplicate import statement
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home} />
-        <Route path='/admin' element={Admin}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path='/admin' element={<Admin></Admin>} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/Pages/Admin/Admin.jsx
+++ frontend/src/Pages/Admin/Admin.jsx
@@ -1,9 +1,8 @@
 
-function Admin() {
-  return (
-    <div>
-      <h1>Admin</h1>
-    </div>
-  );
+export default function Admin() {
+    return (
+        <div>
+        <h1>Admin</h1>
+        </div>
+    );
 }
-export default Admin;
